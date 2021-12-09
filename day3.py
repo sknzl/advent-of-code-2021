b66800004e35699dfe287566d76ff0ecbb5d832f
sknzl <swenkuenzel@gmail.com>
2021-12-09 00:30:52 +0000

    day 3 part two

--- day3.py
+++ day3.py
@@ -1,4 +1,6 @@
+from pdb import set_trace
 from aocd import get_data
+from collections import Counter
 
 
 def return_data():
@@ -22,5 +24,46 @@ def part_a():
     return int(gamma, 2) * int(epsilon, 2)
 
 
+def part_b():
+    data = return_data()
+    result_o2 = extract_life_support(data)
+    result_co2 = extract_life_support(data, k=1)
+    return int("".join([str(elem) for elem in result_o2]), 2) * int(
+        "".join([str(elem) for elem in result_co2]), 2
+    )
+
+
+def extract_life_support(data, k=0):
+    for index in range(len(data[0])):
+        transposed_data = list(map(list, zip(*data)))
+        counter = Counter(transposed_data[index])
+        if len(data) == 1:
+            break
+        elif counter[1] > counter[0]:
+            remove_list = [
+                position
+                for position, bit in enumerate(transposed_data[index])
+                if bit == 0 ^ k
+            ]
+            data = [i for j, i in enumerate(data) if j not in remove_list]
+        elif counter[0] > counter[1]:
+            remove_list = [
+                position
+                for position, bit in enumerate(transposed_data[index])
+                if bit == 1 ^ k
+            ]
+            data = [i for j, i in enumerate(data) if j not in remove_list]
+        elif counter[0] == counter[1]:
+            remove_list = [
+                position
+                for position, bit in enumerate(transposed_data[index])
+                if bit == 0 ^ k
+            ]
+            data = [i for j, i in enumerate(data) if j not in remove_list]
+
+    return data[0]
+
+
 if __name__ == "__main__":
     print(part_a())
+    print(part_b())
